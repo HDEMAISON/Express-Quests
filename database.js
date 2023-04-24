89d1eaa3961cccca2b4d0c038c274c9b242aea1d
Henry DEMAISON <henry31150@hotmail.fr>
2023-04-24 16:06:27 +0200

    quest done

---
+++ database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const mysql = require("mysql2/promise");
+
+const database = mysql.createPool({
+    host: process.env.DB_HOST, // address of the server
+    port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the APP_PORT !
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+  });
+  
+  module.exports = database
