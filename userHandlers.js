c94580dbc4ae374428bbddb1867382c88e7f1c0e
Henry DEMAISON <henry31150@hotmail.fr>
2023-06-12 16:01:55 +0200

    quete fini

--- userHandlers.js
+++ userHandlers.js
@@ -75,12 +75,12 @@ const postUser = (req, res) => {
 
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
-  const { firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email, city, language, hashedPassword } = req.body;
 
   database
     .query(
-      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
-      [firstname, lastname, email, city, language, id]
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?, hashedPassword = ?",
+      [firstname, lastname, email, city, language, id, hashedPassword]
     )
     .then(([result]) => {
       if (result.affectedRows === 0) {
